f42a9462b944c73609fdfb3a66fb27310ec3c13c
0fflines <6182201102@student.unpar.ac.id>
2024-11-20 14:37:53 +0700

    Upload Files

---
+++ Client.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private DataOutputStream out;
+    private BufferedReader in;
+    private Scanner sc;
+
+    public Client(){
+        try{
+            socket = new Socket("localhost", 3030);
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            sc = new Scanner(System.in);
+            System.out.println(in.readLine());
+            writeMessage();
+            close();
+        }catch(IOException e){
+            e.printStackTrace();
+        } 
+    }
+
+    private void writeMessage() throws IOException{
+        String line;
+        System.out.println(in.readLine());
+        while(!(line = sc.nextLine()).equals(Server.STOP_STRING)){
+            out.writeUTF(line);
+            out.flush();
+
+            String serverResponse = in.readLine();
+            System.out.println(serverResponse);
+            System.out.println(in.readLine());
+        }
+        close();
+    }
+
+    private void close() throws IOException{
+        in.close();
+        out.close();
+        sc.close();
+        socket.close();
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
+}
---
+++ Server.java
@@ -0,0 +1,56 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private ServerSocket server;
+    private DataInputStream in;
+    private PrintWriter out;
+    public static final int PORT = 3030;
+    public static final String STOP_STRING = ",,";
+    private Socket clientSocket;
+
+    public Server(){
+        try{
+            server = new ServerSocket(PORT);
+            iniConnections();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void iniConnections() throws IOException{
+        System.out.println("Server started");
+        clientSocket = server.accept();
+        System.out.println("Client socket accepted");
+        in = new DataInputStream(clientSocket.getInputStream());
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        out.println("You're now connected to the Server! Type in "+STOP_STRING +" to stop the program");
+        readMessages();
+        close();
+    }
+
+    public void close() throws IOException{
+        in.close();
+        out.close();
+        server.close();
+    }
+
+    public void readMessages() throws IOException{
+        String inputMessage = "";
+        String responseMessage = "";
+        while(!inputMessage.equals(STOP_STRING)){
+            out.print("Type in your name here: ");
+            inputMessage = in.readUTF();
+            responseMessage = "Server Response: Hello " + inputMessage;
+            System.out.println("Recieved from client: "+inputMessage);
+            out.println(responseMessage);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Server();
+    }
+}
