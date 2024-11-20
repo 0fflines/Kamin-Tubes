d612844f1723814cfb715f8b096d6f6053d50890
0fflines <6182201102@student.unpar.ac.id>
2024-11-20 16:51:24 +0700

    Failed BotClient

---
+++ BotClient.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class BotClient {
+    private Socket socket;
+    private DataOutputStream out;
+    private BufferedReader in;
+
+    public BotClient(){
+        try{
+            socket = new Socket("localhost", Server.PORT);
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println(in.readLine());
+            writeMessage();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //while loop g tau kenapa masih g bisa tapi bisa input kai client biasa
+    private void writeMessage() throws IOException{
+        while(true){
+            out.writeUTF("BotInput\n");
+            out.flush();
+
+            String serverResponse = in.readLine();
+            System.out.println(serverResponse);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Client();
+    }
+}
